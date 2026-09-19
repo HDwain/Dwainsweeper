5ad2a0c04d02d67037c0441d2b5929a9c694fc24
HDwain <marvindaebel@gmail.com>
2026-09-19 15:40:08 +0200

    Updatet Field and added mouse detection

--- main.py
+++ main.py
@@ -2,38 +2,58 @@ import pygame
 
 #setup
 pygame.init()
-screenwith, screenheight = 1280,720
+screenwith, screenheight = 600,600
 screen = pygame.display.set_mode((screenwith, screenheight))
 clock = pygame.time.Clock()
-gamefield = pygame.Rect(10, 10, 1000, 700)
-singleTile = pygame.Rect(gamefield.left,gamefield.top,25,25)
-countw = int(gamefield.width / singleTile.width)
-counth = int(gamefield.height / singleTile.height)
-grid = [["Black" for j in range(countw)] for i in range(counth)]
+gamefield = pygame.Rect(50, 50, 500, 500)
+singletile = pygame.Rect(gamefield.left,gamefield.top,50,50)
+count_w = int(gamefield.width / singletile.width) # anzahle quadrate in der breite
+count_h = int(gamefield.height / singletile.height) # anzahl quadrate in der höhe
+
+grid = [[{"mine": False,
+        "aufgedeckt": False,
+        "markiert": False,
+        "nachbarn": 0}
+        for j in range(count_w)] for i in range(count_h)]
 
 running = True
 while running:
+
+    #### Event handler ##############################
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: #Linksklick
+                mouse_x,mouse_y = event.pos
+                mouse_j = (mouse_x - gamefield.left) // singletile.width
+                mouse_i = (mouse_y - gamefield.top) // singletile.height
+                if mouse_i >= 0 and mouse_i < count_h:
+                    if mouse_j >= 0 and mouse_j < count_w:
+                        grid[mouse_i][mouse_j]["aufgedeckt"] = True
+
+    ################### Draw #########################
     screen.fill("white")
+    pygame.draw.rect(screen,color="grey",rect=gamefield,width=2)
+    for i in range(count_h):
+        for j in range(count_w):            
 
-    pygame.draw.rect(screen,"black",gamefield,width=2)
-    singleTile.left = gamefield.left
-    singleTile.top = gamefield.top
+            #### Calculate Single Square ######################
+            tile_x = gamefield.left + j * singletile.width
+            tile_y = gamefield.top + i * singletile.height
+            tile_rect = pygame.Rect(tile_x, tile_y, singletile.width, singletile.height)
 
-    for i in range(counth):
-        for j in range(countw):
-            pygame.draw.rect(screen,color=grid[i][j],rect=singleTile,width=1)
-            singleTile.left += singleTile.width
-            
-        singleTile.left = gamefield.left
-        singleTile.top += singleTile.height
+            #### Draw ##########################################
+            if grid[i][j]["aufgedeckt"] == True:
+                pygame.draw.rect(screen,"white",rect=tile_rect)
+                pygame.draw.rect(screen,"black",rect=tile_rect,width=1)
+            else:
+                pygame.draw.rect(screen,"grey",rect=tile_rect)
+                pygame.draw.rect(screen,"black",rect=tile_rect,width=1)
    
-   
+   #############################################################################################
 
     pygame.display.flip() #Show Screen
     clock.tick(60) # Max FPS 60
     
 pygame.quit()
-
